6bc0f2a22e758e190feb5594dbeb6c34c21f21b3
Takuma Nakajima <penguin.trance.2716@gmail.com>
2013-03-03 02:20:31 +0900

    support replacing symbols

--- anti_full_width.rb
+++ anti_full_width.rb
@@ -3,6 +3,12 @@
 Plugin.create :anti_full_width do
   halfs = ('0'..'9').to_a + ('A'..'Z').to_a + ('a'..'z').to_a
   fulls = ('０'..'９').to_a + ('Ａ'..'Ｚ').to_a + ('ａ'..'ｚ').to_a
+  half_symbols = ' !""#$%&''\'''\'()*+,-./:;<=>?@[\\]^_`{|}~'.split('')
+  full_symbols = '　！“”＃＄％＆‘’（）＊＋，−．／：；＜＝＞？＠［＼］＾＿｀｛｜｝〜'.split('')
+
+  halfs += half_symbols
+  fulls += full_symbols
+
   table = Hash[fulls.zip(halfs)]
 
   filter_show_filter do |msgs|
